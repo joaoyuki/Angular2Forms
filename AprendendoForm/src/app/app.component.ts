9da90a4ca19deacc39b965b5d671f9a31bb3004d
João Felipe Rodrigues de Assis <joao.assis@unicid.edu.br>
2018-05-07 12:00:35 -0300

    Primeiro form criado

--- AprendendoForm/src/app/app.component.ts
+++ AprendendoForm/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit() {
+    
+  }
+
+  onSubmit(form: FormGroup): void{
+    console.log("Valor submetido é válido: ", form.valid);
+    console.log("Valor submetido: ", form.value);
+    console.log("Valor submetido esta correto: ", form.dirty);
+    console.log("Erros: ", form.errors);
+  }
+
   title = 'app';
 }
